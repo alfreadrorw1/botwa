a7e375fdb40f13418cbd21e55699433065d5dbb4
Alfread Rorw <alfreadrorw@gmail.com>
2026-01-13 13:39:05 +0800

    allll

--- Control/Config.js
+++ Control/Config.js
@@ -1,3 +1,9 @@
+// SC PUSH BY LEXZY & SKYZOPEDIA //
+// BASE DEVELOPERS SKYZO t.me/@Xskycode //
+// YT LEXZYMARKET t.me/@Lexzymarket //
+/* # Penting
+   Jangan hapus credits atau nama developer
+   hargai pembuat script ini!*/
 
 
 const {
